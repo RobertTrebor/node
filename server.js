0b569b0a51039bc6fb6f734b896e4a8912492ea6
RobertTrebor <robert@lengsfeld.de>
2015-08-06 15:32:02 +0200

    using mysql on localhost instead

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ REST.prototype.connectMysql = function() {
     var self = this;
     var pool      =    mysql.createPool({
         connectionLimit : 10,
-        host     : 'us-cdbr-iron-east-02.cleardb.net',
-        user     : 'b6c98aad74a22c',
-        password : '2a75abe3',
-        database : 'heroku_1dd38a75e1b5ba5',
+        // host     : 'us-cdbr-iron-east-02.cleardb.net',
+        // user     : 'b6c98aad74a22c',
+        // password : '2a75abe3',
+        // database : 'heroku_1dd38a75e1b5ba5',
+		// host     : 'us-cdbr-iron-east-02.cleardb.net',
+        host     : 'localhost:3306',
+		user     : 'root',
+        password : '',
+        database : 'cimitery',
         debug    :  true
     });
     pool.getConnection(function(err,connection){
